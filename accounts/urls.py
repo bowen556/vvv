9db0dde871e45935553ef26365bec8ced600367d
t17ss19 <t17ss19@abdn.ac.uk>
2020-07-02 16:41:31 +0100

    View Locations Page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('locations/', views.locations, name="locations"),
+    path('viewlocations/<str:pk>/', views.viewlocations, name="viewlocations"),
     path('users/', views.users, name="users"),
     path('admin/auth/user/add/', views.createUsers, name="create_users"),
     path('admin/accounts/entry/add/', views.createLocations, name="create_locations"),
