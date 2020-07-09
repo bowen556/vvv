3028085282632fa8047c2a3aa4c5d31b5163534c
t17ss19 <t17ss19@abdn.ac.uk>
2020-07-09 19:50:08 +0100

    Multiple image upload functionality for Admin user - Location Entry

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from accounts.models import Entry
 
 
 
@@ -9,3 +10,13 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class EntryForm(forms.ModelForm):
+
+    class Meta:
+        model = Entry
+        fields = ('number_of_images',)
+        widgets = {
+            'number_of_images': forms.Select(choices=Entry.CHOICES)
+        }
